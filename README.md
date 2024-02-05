280d6ddfd1b3a92dd2b8bd81818496f6d81276ed
CompositeMan <cemtunaboylu9@gmail.com>
2024-02-05 22:17:48 +0300

    move todo into readme

---
+++ README.md
@@ -0,0 +1,6 @@
+TODOs: 
+    1. Verbosity level for logs
+    2. use timeouts in asyncs
+    3. guard the secret key
+    4. solve bigint problem
+
--- wallet.ts
+++ wallet.ts
@@ -31,15 +31,6 @@ function extractConfigsFromParameters(): Configuration {
     }
 }
 
-/*
-    TODOs: 
-        1. Verbosity level for logs
-        2. use timeouts in asyncs
-        3. guard the secret key
-        4. solve bigint problem
-*/
-
-
 function createKeyPair(): Keypair {
     return Keypair.generate();
 }
