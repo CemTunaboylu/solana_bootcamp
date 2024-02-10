6d245713ef53c8b352001e6d52fbfd981503e3a0
CompositeMan <cemtunaboylu9@gmail.com>
2024-02-10 15:19:54 +0300

    add base methods into wallet.ts

--- wallet.ts
+++ wallet.ts
@@ -1,17 +1,59 @@
-import { Connection, LAMPORTS_PER_SOL, SystemProgram, Transaction, Keypair, PublicKey, TransactionSignature, sendAndConfirmTransaction } from "@solana/web3.js";
+import { Connection, LAMPORTS_PER_SOL, SystemProgram, Transaction, BlockhashWithExpiryBlockHeight, PublicKey, TransactionSignature, sendAndConfirmTransaction, SignatureResult, VersionedTransaction } from '@solana/web3.js';
 
-import { log } from "./logging";
-import { Wallet } from "./interfaces";
+import { logWithTrace } from "./logging";
+import { Wallet, TransactionConfirmer } from "./interfaces";
 
 async function New(): Promise<Wallet> {
     throw new Error("not implemented");
 }
 
-async function Transer(fromWallet: Wallet, toPublicKey: PublicKey, lamports: number) {
-    throw new Error("not implemented");
+export async function prepareTransaction(
+    fromWallet: Wallet,
+    toPublicKey: PublicKey,
+    lamports: number,
+    recentBlockHash?: BlockhashWithExpiryBlockHeight,
+    lastValidBlockHeight?: number
+) {
+    let currentBalanceInLamports = await fromWallet.getBalance();
+    const isBalanceNotEnough = lamports >= currentBalanceInLamports;
+    if (isBalanceNotEnough) {
+        const errMessage = `Wallet ${fromWallet.getIdentifier()} does not have requested amount of lamports(${lamports}) to send to ${toPublicKey}`
+        throw new Error(errMessage);
+    }
+
+    return new Transaction().add(
+        SystemProgram.transfer({
+            fromPubkey: fromWallet.getPublicKey(),
+            toPubkey: toPublicKey,
+            lamports: lamports,
+            // if recentBlockHash is given, provide it
+            ... (recentBlockHash != null && {
+                recentBlockHash: recentBlockHash,
+            }),
+            // if lastValidBlockHeight is given, provide it
+            ... (lastValidBlockHeight != null && {
+                lastValidBlockHeight: lastValidBlockHeight,
+            })
+        })
+    )
 }
 
-async function airdrop(connection: Connection, toWallet: Wallet, lamports: number, confirmer?: TransactionConfirmer) {
+async function Transfer(connection: Connection, fromWallet: Wallet, toPublicKey: PublicKey, lamports: number) {
+    const logTrace = 'Transfer'
+    try {
+        const recentBlockHash: BlockhashWithExpiryBlockHeight = await connection.getLatestBlockhash()
+        let tx: Transaction = await prepareTransaction(fromWallet, toPublicKey, lamports, recentBlockHash)
+        let signatures: Array<Uint8Array> = new Array<Uint8Array>(1);
+        signatures[0] = fromWallet.sign(tx)
+        const txMessage = tx.compileMessage();
+        const versionedTransaction: VersionedTransaction = new VersionedTransaction(txMessage, signatures)
+        connection.sendTransaction(versionedTransaction);
+    } catch (error) {
+        logWithTrace(logTrace, error)
+    }
+}
+
+export async function airdrop(connection: Connection, toWallet: Wallet, lamports: number, confirmer?: TransactionConfirmer) {
     const logTrace = 'airdrop'
     const airdropSignature = await connection.requestAirdrop(
         toWallet.getPublicKey(),
@@ -19,10 +61,6 @@ async function airdrop(connection: Connection, toWallet: Wallet, lamports: numbe
     )
     if (confirmer) {
         const signatureResult: SignatureResult = await confirmer.confirm(airdropSignature);
-        logWithTrace(logTrace, "confirmation result - ", signatureResult)
+        logWithTrace(logTrace, `confirmation result - ${signatureResult} : ${["successful", "failed"][+(signatureResult.err != null)]}`)
     }
-}
-
-async function balance(ofWallet: Wallet): Promise<number> {
-    throw new Error("not implemented");
 }
