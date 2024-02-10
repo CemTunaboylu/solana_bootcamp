2c131d3db2e33adfd24ba4e7da648d9df9aafe8e
CompositeMan <cemtunaboylu9@gmail.com>
2024-02-10 23:23:38 +0300

    refactor transaction and transfer functions wallet.ts

--- wallet.ts
+++ wallet.ts
@@ -7,10 +7,23 @@ async function New(): Promise<Wallet> {
     throw new Error("not implemented");
 }
 
-type TransactionOrError = {
-    tx: Transaction | null,
-    err: Error | null
-}
+class TransactionOrError {
+    tx: Transaction | null;
+    err: Error | null;
+
+    constructor(tx: Transaction | null, err: Error | null) {
+        this.tx = tx;
+        this.err = err;
+    }
+
+    isTransactionFormationFailed(): boolean {
+        return (null != this.err) || (null == this.tx)
+    }
+
+    getTransaction(): Transaction {
+        return this.tx as Transaction
+    }
+};
 
 export async function prepareTransaction(
     fromWallet: Wallet,
@@ -19,17 +32,15 @@ export async function prepareTransaction(
     recentBlockHash: BlockhashWithExpiryBlockHeight,
     balanceCheck: boolean = false
 ): Promise<TransactionOrError> {
-    let txOrErr: TransactionOrError = { tx: null, err: null }
+    // TODO: this currently is not guaranteed to be consistent
     if (balanceCheck) {
         let currentBalanceInLamports = await fromWallet.getBalance();
         const isBalanceNotEnough = lamports >= currentBalanceInLamports;
         if (isBalanceNotEnough) {
             const errMessage = `Wallet ${fromWallet.getIdentifier()} does not have requested amount of lamports(${lamports}) to send to ${toPublicKey}`
-            txOrErr.err = new Error(errMessage);
-            return txOrErr;
+            return new TransactionOrError(null, new Error(errMessage))
         }
     }
-
     let transaction = new Transaction({
         feePayer: fromWallet.getPublicKey(),
         blockhash: recentBlockHash.blockhash,
@@ -41,21 +52,30 @@ export async function prepareTransaction(
         lamports: lamports,
     });
     transaction.add(transferInstruction);
-    txOrErr.tx = transaction
-    return txOrErr;
+    return new TransactionOrError(transaction, null)
+}
+
+function createSignatures(wallets: Wallet[], tx: Transaction): Array<Uint8Array> {
+    let signatures: Array<Uint8Array> = new Array<Uint8Array>(wallets.length);
+    for (let index = 0; index < wallets.length; index++) {
+        const wallet = wallets[index];
+        signatures[index] = wallet.sign(tx)
+    }
+    return signatures;
 }
 
 // TODO: make it able to take tx as a parameter
+// TODO: add preTransfer and postTransfer
 export async function transfer(connection: Connection, fromWallet: Wallet, toPublicKey: PublicKey, lamports: number): Promise<TransactionSignature> {
     const logTrace = 'Transfer'
     let txSignature: TransactionSignature = "";
     try {
         const recentBlockHash: BlockhashWithExpiryBlockHeight = await connection.getLatestBlockhash()
-        let signatures: Array<Uint8Array> = new Array<Uint8Array>(1);
         let txOrError: TransactionOrError = await prepareTransaction(fromWallet, toPublicKey, lamports, recentBlockHash)
-        if (null != txOrError.err || null == txOrError.tx) return txSignature;
-        let tx: Transaction = txOrError.tx;
-        signatures[0] = fromWallet.sign(tx)
+        if (txOrError.isTransactionFormationFailed()) return txSignature; // return empty hash
+        // TODO: check if this tx has been seen before i.e. handle duplicates
+        const tx: Transaction = txOrError.getTransaction();
+        const signatures = createSignatures([fromWallet], tx)
         const txMessage = tx.compileMessage();
         const versionedTransaction: VersionedTransaction = new VersionedTransaction(txMessage, signatures)
         txSignature = await connection.sendTransaction(versionedTransaction);
