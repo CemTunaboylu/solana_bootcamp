02dae004b0533f47b253e3b7aaf00bfb6f547f1e
CompositeMan <cemtunaboylu9@gmail.com>
2024-02-10 18:27:08 +0300

    transaction and transfer fixes

--- wallet.ts
+++ wallet.ts
@@ -1,4 +1,4 @@
-import { Connection, LAMPORTS_PER_SOL, SystemProgram, Transaction, BlockhashWithExpiryBlockHeight, PublicKey, TransactionSignature, sendAndConfirmTransaction, SignatureResult, VersionedTransaction } from '@solana/web3.js';
+import { Connection, SystemProgram, Transaction, BlockhashWithExpiryBlockHeight, PublicKey, TransactionSignature, SignatureResult, VersionedTransaction } from '@solana/web3.js';
 
 import { logWithTrace } from "./logging";
 import { Wallet, TransactionConfirmer } from "./interfaces";
@@ -11,46 +11,52 @@ export async function prepareTransaction(
     fromWallet: Wallet,
     toPublicKey: PublicKey,
     lamports: number,
-    recentBlockHash?: BlockhashWithExpiryBlockHeight,
-    lastValidBlockHeight?: number
-) {
-    let currentBalanceInLamports = await fromWallet.getBalance();
-    const isBalanceNotEnough = lamports >= currentBalanceInLamports;
-    if (isBalanceNotEnough) {
-        const errMessage = `Wallet ${fromWallet.getIdentifier()} does not have requested amount of lamports(${lamports}) to send to ${toPublicKey}`
-        throw new Error(errMessage);
+    recentBlockHash: BlockhashWithExpiryBlockHeight,
+    balanceCheck: boolean = false
+): Promise<Transaction> {
+    if (balanceCheck) {
+        let currentBalanceInLamports = await fromWallet.getBalance();
+        const isBalanceNotEnough = lamports >= currentBalanceInLamports;
+        if (isBalanceNotEnough) {
+            const errMessage = `Wallet ${fromWallet.getIdentifier()} does not have requested amount of lamports(${lamports}) to send to ${toPublicKey}`
+            throw new Error(errMessage);
+        }
     }
 
-    return new Transaction().add(
-        SystemProgram.transfer({
-            fromPubkey: fromWallet.getPublicKey(),
-            toPubkey: toPublicKey,
-            lamports: lamports,
-            // if recentBlockHash is given, provide it
-            ... (recentBlockHash != null && {
-                recentBlockHash: recentBlockHash,
-            }),
-            // if lastValidBlockHeight is given, provide it
-            ... (lastValidBlockHeight != null && {
-                lastValidBlockHeight: lastValidBlockHeight,
-            })
-        })
-    )
+    // Create a new transaction
+    // let transaction = createTransaction({ version: 0 });
+    let transaction = new Transaction({
+        feePayer: fromWallet.getPublicKey(),
+        blockhash: recentBlockHash.blockhash,
+        lastValidBlockHeight: recentBlockHash.lastValidBlockHeight
+    });
+    // tx => appendTransactionInstruction(createTransferInstruction(myAddress, toAddress, amountInLamports), tx),
+    // let transaction = new Transaction();
+    const transferInstruction = SystemProgram.transfer({
+        fromPubkey: fromWallet.getPublicKey(),
+        toPubkey: toPublicKey,
+        lamports: lamports,
+    });
+    transaction.add(transferInstruction);
+    return transaction
 }
 
-async function Transfer(connection: Connection, fromWallet: Wallet, toPublicKey: PublicKey, lamports: number) {
+// TODO: make it able to take tx as a parameter
+export async function transfer(connection: Connection, fromWallet: Wallet, toPublicKey: PublicKey, lamports: number): Promise<TransactionSignature> {
     const logTrace = 'Transfer'
+    let txSignature: TransactionSignature = "";
     try {
         const recentBlockHash: BlockhashWithExpiryBlockHeight = await connection.getLatestBlockhash()
-        let tx: Transaction = await prepareTransaction(fromWallet, toPublicKey, lamports, recentBlockHash)
         let signatures: Array<Uint8Array> = new Array<Uint8Array>(1);
+        let tx: Transaction = await prepareTransaction(fromWallet, toPublicKey, lamports, recentBlockHash)
         signatures[0] = fromWallet.sign(tx)
         const txMessage = tx.compileMessage();
         const versionedTransaction: VersionedTransaction = new VersionedTransaction(txMessage, signatures)
-        connection.sendTransaction(versionedTransaction);
+        txSignature = await connection.sendTransaction(versionedTransaction);
     } catch (error) {
         logWithTrace(logTrace, error)
     }
+    return txSignature;
 }
 
 export async function airdrop(connection: Connection, toWallet: Wallet, lamports: number, confirmer?: TransactionConfirmer) {
@@ -61,6 +67,6 @@ export async function airdrop(connection: Connection, toWallet: Wallet, lamports
     )
     if (confirmer) {
         const signatureResult: SignatureResult = await confirmer.confirm(airdropSignature);
-        logWithTrace(logTrace, `confirmation result - ${signatureResult} : ${["successful", "failed"][+(signatureResult.err != null)]}`)
+        logWithTrace(logTrace, `confirmation result - ${signatureResult} : ${["successful", "failed"][+(signatureResult.err != null)]} `)
     }
 }
