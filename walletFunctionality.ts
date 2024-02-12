98844a435459a0dd1622dec4b9ff2f3319c32bf3
CompositeMan <cemtunaboylu9@gmail.com>
2024-02-12 22:35:43 +0300

    versioned and unsigned transaction handling

--- walletFunctionality.ts
+++ walletFunctionality.ts
@@ -7,11 +7,20 @@ async function New(): Promise<Wallet> {
     throw new Error("not implemented");
 }
 
+export class UnsignedTransaction {
+    tx: Transaction;
+    wallet: Wallet;
+    constructor(tx: Transaction, w: Wallet) {
+        this.tx = tx
+        this.wallet = w
+    }
+}
+
 export class TransactionOrError {
-    tx: Transaction | null;
+    tx: VersionedTransaction | UnsignedTransaction | null;
     err: Error | null;
 
-    constructor(tx: Transaction | null, err: Error | null) {
+    constructor(tx: VersionedTransaction | UnsignedTransaction | null, err: Error | null) {
         this.tx = tx;
         this.err = err;
     }
@@ -20,8 +29,8 @@ export class TransactionOrError {
         return (null != this.err) || (null == this.tx)
     }
 
-    getTransaction(): Transaction {
-        return this.tx as Transaction
+    getTransaction(): VersionedTransaction | UnsignedTransaction | null {
+        return this.tx
     }
 };
 
@@ -47,10 +56,22 @@ export async function prepareTransaction(
         lamports: lamports,
     });
     transaction.add(transferInstruction);
-    return new TransactionOrError(transaction, null)
+    let validTransaction: UnsignedTransaction | VersionedTransaction
+    if (sign) {
+        validTransaction = createVersionedTransactionFrom(fromWallet, transaction);
+    } else {
+        validTransaction = new UnsignedTransaction(transaction, fromWallet);
+    }
+    return new TransactionOrError(validTransaction, null)
 }
 
-function createSignatures(wallets: Wallet[], tx: Transaction): Array<Uint8Array> {
+export function createVersionedTransactionFrom(fromWallet: Wallet, tx: Transaction) {
+    const signatures = createSignatures([fromWallet], tx)
+    const txMessage = tx.compileMessage();
+    return new VersionedTransaction(txMessage, signatures)
+}
+
+export function createSignatures(wallets: Wallet[], tx: Transaction): Array<Uint8Array> {
     let signatures: Array<Uint8Array> = new Array<Uint8Array>(wallets.length);
     for (let index = 0; index < wallets.length; index++) {
         const wallet = wallets[index];
@@ -75,11 +96,34 @@ export async function transfer(
         let txOrError: TransactionOrError = await prepareTransaction(fromWallet, toPublicKey, lamports, recentBlockHash, balanceChecker)
         if (txOrError.isTransactionFormationFailed()) return txSignature; // return empty hash
         // TODO: check if this tx has been seen before i.e. handle duplicates
-        const tx: Transaction = txOrError.getTransaction();
-        const signatures = createSignatures([fromWallet], tx)
-        const txMessage = tx.compileMessage();
-        const versionedTransaction: VersionedTransaction = new VersionedTransaction(txMessage, signatures)
-        txSignature = await connection.sendTransaction(versionedTransaction);
+        let tx: VersionedTransaction | UnsignedTransaction | null = txOrError.getTransaction();
+        if (null == tx) return txSignature; // should never happen since we checked for isTransactionFormationFailed above
+        if (tx instanceof UnsignedTransaction) {
+            tx = createVersionedTransactionFrom(tx.wallet, tx.tx)
+        }
+        txSignature = await connection.sendTransaction(tx);
+    } catch (error) {
+        logWithTrace(logTrace, error)
+    }
+    return txSignature;
+}
+
+// TODO: add preTransfer and postTransfer
+export async function transferTransaction(
+    connection: Connection,
+    transaction: VersionedTransaction | UnsignedTransaction,
+): Promise<TransactionSignature> {
+    const logTrace = 'transferTransaction'
+    let txSignature: TransactionSignature = "";
+    try {
+        const recentBlockHash: BlockhashWithExpiryBlockHeight = await connection.getLatestBlockhash();
+        if (transaction instanceof UnsignedTransaction) {  // needs signature
+            if (null == transaction.tx || undefined == transaction.wallet) {
+                return txSignature; // return empty, something is wrong
+            }
+            transaction = createVersionedTransactionFrom(transaction.wallet, transaction.tx)
+        }
+        txSignature = await connection.sendTransaction(transaction);
     } catch (error) {
         logWithTrace(logTrace, error)
     }
