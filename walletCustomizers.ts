b0420eef28903e1889d6561d67edbdd4515927a5
CompositeMan <cemtunaboylu9@gmail.com>
2024-02-14 00:51:24 +0300

    add default wallet customizers

---
+++ walletCustomizers.ts
@@ -0,0 +1,19 @@
+import { Keypair, PublicKey } from '@solana/web3.js';
+
+import { WalletCustomizerFunction, Wallet } from './interfaces'
+
+export function WithIdentifier(identifier: string): WalletCustomizerFunction {
+    return (wallet: Wallet) => {
+        wallet.setIdentifier(identifier)
+    }
+}
+
+export function WithKeypair(keypair: Keypair): WalletCustomizerFunction {
+    return (wallet: Wallet) => {
+        if (wallet.getIdentifier() !== null) {
+            throw new Error('Cannot set keypairs of a wallet without identifier')
+        }
+        // setting the private key automatically updates the public key
+        wallet.setPrivateKey(keypair.secretKey);
+    }
+}
